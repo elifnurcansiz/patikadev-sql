5aa4d3af204c8bfa6333ae7456ed272e5edb7e58
Elifnur Cansız <81492504+elifnurcansiz@users.noreply.github.com>
2024-07-13 23:40:17 +0300

    Create odev8-8.sql

---
+++ odev8-8.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee (
+  id integer,
+  name varchar(50),
+  birthday Date
+  email VARCHAR(100) 
+  );
+
+
+insert into employee (id,name,bithday,email) values((1, 'Loren Arnot', '1991-01-08', 'larnot0@usda.gov')
